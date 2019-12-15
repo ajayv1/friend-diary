c6bf29b3f792c9f6452a2e2d6c18986588d8e0e2
Ajay <akverma@gwynniebee.com>
2019-12-16 01:53:33 +0530

    Updates

---
+++ precache-manifest.eb9b4a0b1918e884b409bdb164b9ebe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f5fb25c6a19226c522a5a73a23dde6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fde0a18497ea44a157db",
+    "url": "/static/css/main.47664e35.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fde0a18497ea44a157db",
+    "url": "/static/js/main.b0cd8cf2.chunk.js"
+  },
+  {
+    "revision": "8dad691eb5b115f4aeab",
+    "url": "/static/js/runtime-main.d31eab9b.js"
+  }
+]);

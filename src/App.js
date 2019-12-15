110cd29e316ec40db2bfe43d284c6a0fac7be2fd
Ajay <akverma@gwynniebee.com>
2019-12-16 01:32:03 +0530

    Added friend diary file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SearchBox } from './component/search-box/search-box.component';
+import { CardList } from './component/card-list/card-list.component';
+
+class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      friends: [],
+      searchFields: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => this.setState({ friends: users}))
+  }
+
+  render() {
+    const { friends, searchFields } = this.state;
+    const filteredFriends = friends.filter(friend => 
+        friend.name.toLowerCase().includes(searchFields.toLowerCase())
+      );
+
+    return (  
+      <div className="App">
+        <h1>Friend Diary</h1>
+        <SearchBox 
+          placeholder="search friends" 
+          handleChange = {
+            e => {
+              this.setState({searchFields: e.target.value})
+            } 
+          }
+        ></SearchBox>
+        <CardList friends={filteredFriends}></CardList> 
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './card-list.styles.css';
+import { Card } from '../card/card.component';
+export const CardList = props => {
+  return <div className="card-list">
+    {
+      props.friends.map((friend) => <Card key={ friend.id} friend={ friend }></Card>)
+    } 
+  </div>;
+} 
